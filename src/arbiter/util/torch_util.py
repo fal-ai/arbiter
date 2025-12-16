28c006acbfb326957c1a519f0ce3685e54a48d30
Benjamin Paine <painebenjamin@gmail.com>
2025-12-16 16:08:38 -0500

    precommit

--- src/arbiter/util/torch_util.py
+++ src/arbiter/util/torch_util.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import torch
 
+
 def get_device() -> "torch.device":
     """
     Get the device to use for torch operations.
@@ -17,7 +18,9 @@ def get_device() -> "torch.device":
         return torch.device("cpu")
 
 
-def get_device_and_dtype_from_module(module: "torch.nn.Module") -> tuple["torch.device", "torch.dtype"]:
+def get_device_and_dtype_from_module(
+    module: "torch.nn.Module",
+) -> tuple["torch.device", "torch.dtype"]:
     """
     Get the device and dtype to use for a module.
     """
